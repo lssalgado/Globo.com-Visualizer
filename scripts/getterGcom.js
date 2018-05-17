ae3a1a358a40664c66e7e126990a7946caaada72
lssalgado <lucas.salgado@uniriotec.br>
2018-05-17 16:37:15 -0300

    System is now able to get all the necessary information about each repository.

--- scripts/getterGcom.js
+++ scripts/getterGcom.js
@@ -1,17 +1,50 @@
+var url_repos_Gcom = "https://api.github.com/repos/globocom/";
 var actual_page = 1;
 var actual_element = 0;
 var gcom_repos = 0;
 var gcom_repos_array = [];
 var gcom_repos_json = {};
+var star_amount = 0;
+var fork_amount = 0;
+var contributors_amount = 0;
+var commits_per_week = [];
 
 function getReposData() {
-    console.log(document.getElementById("repos_select").value);
+
+    reposToGet = document.getElementById("repos_select").value;
+    console.log(reposToGet);
+
+    $.get(url_repos_Gcom + reposToGet, function (data) {
+        star_amount = data.stargazers_count;
+        fork_amount = data.forks;
+    })
+        .done(function () {
+            contributors_amount = 0;
+            $.get(url_repos_Gcom + reposToGet + "/contributors", function (data) {
+                data.forEach(function () {
+                    contributors_amount++;
+                });
+            })
+                .done(function () {
+                    console.log("contributors_amount = " + contributors_amount);
+
+                    $.get(url_repos_Gcom + reposToGet + "/stats/participation", function (data) {
+
+                        commits_per_week = data.all;
+                        console.log(commits_per_week);
+
+                    })
+                });
+        });
+
+
+
 
 
 }
 
 function createSelect() {
-    
+
 
     for (var i = 0; i < gcom_repos_array.length; i++) {
         var opt = gcom_repos_array[i].name;
@@ -20,7 +53,7 @@ function createSelect() {
         ele.value = opt;
         document.getElementById("repos_select").appendChild(ele);
     }
-    
+
     var temp_opt = document.getElementById("temp_opt")
     temp_opt.parentNode.removeChild(temp_opt);
 
